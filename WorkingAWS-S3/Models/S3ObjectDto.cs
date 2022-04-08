b73bfc7ea7a9b3565ef3c2555ba7fddbea37e4ed
Fabio Galante Mans <fgamans@hotmail.com>
2022-04-08 15:37:51 -0300

    Add project files.

---
+++ WorkingAWS-S3/Models/S3ObjectDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingAWS_S3.Models
+{
+    public class S3ObjectDto
+    {
+        public string? Name { get; set; }
+        public string? PresignedUrl { get; set; }
+    }
+}
